d9906f59965da97c41c6dca19f0237711a3b512a
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-04-30 23:53:47 +0900

    html, css, javascript 연습

--- Html,Css/precamp/final/final.js
+++ Html,Css/precamp/final/final.js
@@ -1,4 +1,36 @@
+let lock = false;
+
 const authSend = () => {
-    let num = StringMath.floor(1000000 * Math.random());
-    document.getElementById("auth_send").getElementsByTagName("span").value = num;
+
+    if(lock === false){
+        lock = true;
+    
+        var num = String(Math.floor(1000000 * Math.random())).padStart(6,"0");
+        document.getElementById("auth_send").innerText = num;
+        
+        var time = 180;
+        
+        setInterval(function(){
+
+            if(time >= 0){
+                var min = Math.floor(time / 60);
+                var sec = String(time % 60).padStart(2,"0");
+                document.getElementById("auth_done").innerText = min + ":" + sec;
+                time = time - 1;
+            }else{
+                lock = false;
+                clearInterval(send)
+            }
+
+        },1000)
+    }
+
+}
+
+const authDone = () => {
+    alert("가입을 축하합니다.");
+}
+
+const changeFocus1 = () =>{
+    document.getElementById("")
 }
