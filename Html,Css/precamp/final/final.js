60fb169b0f57e16bb5de874b422e49b479a96ddc
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-05-02 23:18:54 +0900

    Javascript 연습

--- Html,Css/precamp/final/final.js
+++ Html,Css/precamp/final/final.js
@@ -2,28 +2,40 @@ let lock = false;
 
 const authSend = () => {
 
-    if(lock === false){
-        lock = true;
+    if(ph1.length === 3 && ph2.length === 4 && ph3.length === 4){
+        document.getElementById("send_auth_button").disabled = false;
+        document.getElementById("send_auth_button").style.color = "#0068FF"
+
+        if(lock === false){
+            lock = true;
+        
+            var num = String(Math.floor(1000000 * Math.random())).padStart(6,"0");
+            document.getElementById("auth_send").innerText = num;
+            
+            var time = 180;
+            
+            setInterval(function(){
     
-        var num = String(Math.floor(1000000 * Math.random())).padStart(6,"0");
-        document.getElementById("auth_send").innerText = num;
-        
-        var time = 180;
-        
-        setInterval(function(){
+                if(time >= 0){
+                    var min = Math.floor(time / 60);
+                    var sec = String(time % 60).padStart(2,"0");
+                    document.getElementById("auth_done").innerText = min + ":" + sec;
+                    time = time - 1;
+                }else{
+                    lock = false;
+                    clearInterval(send)
+                }
+    
+            },1000)
+        }
 
-            if(time >= 0){
-                var min = Math.floor(time / 60);
-                var sec = String(time % 60).padStart(2,"0");
-                document.getElementById("auth_done").innerText = min + ":" + sec;
-                time = time - 1;
-            }else{
-                lock = false;
-                clearInterval(send)
-            }
-
-        },1000)
+    }else{
+        alert("비어있는 번호가 있습니다.")
     }
+
+    
+
+    
 
 }
 
@@ -43,4 +55,8 @@ const changeFocus2 = () =>{
     if(ph1.length === 4){
         document.getElementById("ph3").focus();
     }
+}
+
+const checkFullPhone = () =>{
+    
 }
