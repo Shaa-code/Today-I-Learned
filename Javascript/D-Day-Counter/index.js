1fc651c26d41931299fd4fe074aab941190bb903
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-05-25 15:58:56 +0900

    Javascript 연습 (타이머)

--- Javascript/D-Day-Counter/index.js
+++ Javascript/D-Day-Counter/index.js
@@ -1,34 +1,42 @@
-const consoleTool = () =>{
-    console.log("실행");
-}
+const consoleTool = () => {
+  console.log("실행");
+};
 
-const dateFormMaker = function() {
+const dateFormMaker = function () {
+  const inputYear = document.querySelector("#target-year-input").value;
+  const inputMonth = document.querySelector("#target-month-input").value;
+  const inputDate = document.querySelector("#target-date-input").value;
 
-    const inputYear = document.querySelector('#target-year-input').value;
-    const inputMonth = document.querySelector('#target-month-input').value;
-    const inputDate = document.querySelector('#target-date-input').value;
+  // const dateFormat = inputYear + '-' + inputMonth + '-' + inputDate;
+  const dateFormat = `${inputYear}-${inputMonth}-${inputDate}`;
 
-    // const dateFormat = inputYear + '-' + inputMonth + '-' + inputDate;
-    const dateFormat = `${inputYear}-${inputMonth}-${inputDate}`;
+  return dateFormat;
+};
 
-    return dateFormat;
-}
+const counterMaker = function () {
+  const targetDateInput = dateFormMaker();
+  console.log(targetDateInput);
+  const nowDate = new Date();
+  const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);
+  const remain = (targetDate - nowDate) / 1000;
 
-const counterMaker = function(){
+  if (remain <= 0) {
+    console.log("타이머 종료");
+  } else if (isNaN(remain)) {
+    console.log("유효한 시간대가 아닙니다.");
+    // 만약 잘못된 날짜가 들어왔다면, 유효한 시간대가 아니다.
+  }
 
-    const targetDateInput = dateFormMaker();
-    console.log(targetDateInput);
-    const nowDate = new Date();
-    const targetDate = new Date();
-    const remain = (targetDate - nowDate) / 1000;
+  const date = Math.floor(remain / 3600 / 24);
+  const dateHour = Math.floor((remain / 3600) % 24);
+  const dateMin = Math.floor(remain / 60) % 60;
+  const dateSec = Math.floor(remain) % 60;
 
-    const date = Math.floor(remain / 3600 / 24);
-    const dateHour = Math.floor(remain / 3600 % 24);
-    const dateMin = Math.floor(remain / 60) % 60;
-    const dateSec = Math.floor(remain) % 60;
+  document.getElementById("target-selector");
+  document.getElementById("target-date-input");
+  document.getElementById("target-");
 
-    console.log(dateHour);
-    console.log(dateMin);
-    console.log(dateSec);
-
-}
+  console.log(dateHour);
+  console.log(dateMin);
+  console.log(dateSec);
+};
