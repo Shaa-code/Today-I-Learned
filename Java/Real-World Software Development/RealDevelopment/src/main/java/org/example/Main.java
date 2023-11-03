83059daeddc41e9c674284f7977f03cdb4f8594c
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-11-03 17:52:44 +0900

    implemented by Me

--- Java/Real-World Software Development/RealDevelopment/src/main/java/org/example/Main.java
+++ Java/Real-World Software Development/RealDevelopment/src/main/java/org/example/Main.java
@@ -1,20 +1,17 @@
 package org.example;
 
-import org.example.util.BankCsvService;
-import org.example.util.CsvReader;
-import java.io.UnsupportedEncodingException;
-import java.util.List;
+import org.example.util.Bank.BankStatementAnalyzer;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        CsvReader csvReader = CsvReader.getInstance();
-        BankCsvService bankCsvService = new BankCsvService();
-        List<List<String>> csvList = CsvReader.readCsv("History");
-        System.out.println(csvList);
-        System.out.println(bankCsvService.findAllIncome(csvList));
-        System.out.println(bankCsvService.findAllOutcome(csvList));
-        System.out.println(bankCsvService.countHistoryAtMonth(csvList,1));
-        System.out.println(bankCsvService.findOutcomeTop10(csvList));
-        System.out.println(bankCsvService.findTopOutcomeCategory(csvList));
+    public static void main(String[] args) throws IOException {
+
+        BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+
+        try {
+            bankStatementAnalyzer.analyze("History.csv");
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
     }
 }
