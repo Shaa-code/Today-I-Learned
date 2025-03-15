801c38ba57adc654b193245c6f24392ea2318f75
peom3386 <rtp3386@gmail.com>
2025-03-15 23:36:23 +0900

    TypeScript Learning

---
+++ TypeScript/section02/src/chapter4.ts
@@ -0,0 +1,26 @@
+type User = {
+  id: number;
+  name: string;
+  nickname: string;
+  birth: string;
+  bio: string;
+  location: string;
+};
+
+let user: User = {
+  id: 1,
+  name: "신승윤",
+  nickname: "Yun",
+  birth: "1997.01.01",
+  bio: "bio",
+  location: "부천시",
+};
+
+let user2: User = {
+  id: 2,
+  name: "이정환",
+  nickname: "Nick",
+  birth: "1997.01.01",
+  bio: "bio",
+  location: "부천시",
+};
